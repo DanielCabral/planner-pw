cf1566749a7a24a9995a20ea001e4c306afd01c4
DanielCabral <daniel-cabral14@hotmail.com>
2021-10-16 01:15:19 -0300

    adjust login page web and mobile  with figma

--- src/components/header.js
+++ src/components/header.js
@@ -3,15 +3,14 @@ import {Link} from 'react-router-dom'
 
 export default function Header() {
     return (
-        <div className='h-16 bg-primary flex items-center justify-between'>
-              <div>
+        <div className='h-16 bg-primary flex items-center justify-between '>              
               <Link
                 to='/'
+                className='mx-auto lg:mx-0'
               >
-                  <p className='font-bold text-white ml-4'>Planner</p>
-                </Link>
-              </div>
-              <Link className='px-6 py-2 bg-fourth text-white text-lg rounded-lg mr-4'
+                  <p className='font-bold font-serif text-white text-3xl mx-auto lg:ml-4 '>Planner</p>
+              </Link>              
+              <Link className='hidden lg:block px-16 py-2 bg-secondary text-white text-lg rounded-lg mr-4 transition duration-500 hover:scale-110 hover:text-secondary hover:bg-white hover:border-2 hover:border-secondary'
                 to='login'
               >Login</Link>
           </div>
--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -4,69 +4,38 @@ import Header from '../../components/header';
 
 export default function Home() {
     return (
-       <div className='w-full h-screen'>
-           {/*
-           <nav className="bg-white shadow-lg">
-		<div className="max-w-6xl mx-auto px-4">
-			<div className="flex justify-between">
-				<div className="flex space-x-7">
-					<div>
-						<a href="#" className="flex items-center py-4 px-2" >
-							<img src="logo.png" alt="Logo" classNameName="h-8 w-8 mr-2" />
-							<span className="font-semibold text-gray-500 text-lg"
-								>Navigation</span
-							>
-						</a>
-					</div>
-					<div className="hidden md:flex items-center space-x-1">
-						<a
-							href=""
-							className="py-4 px-2 text-green-500 border-b-4 border-green-500 font-semibold "
-							>Home</a
-						>
-						<a
-							href=""
-							className="py-4 px-2 text-gray-500 font-semibold hover:text-green-500 transition duration-300"
-							>Services</a
-						>
-						<a
-							href=""
-							className="py-4 px-2 text-gray-500 font-semibold hover:text-green-500 transition duration-300"
-							>About</a
-						>
-						<a
-							href=""
-							className="py-4 px-2 text-gray-500 font-semibold hover:text-green-500 transition duration-300"
-							>Contact Us</a
-						>
-					</div>
-				</div>
-			</div>
-		</div>
-	</nav>*/}
+       <div className='w-full h-screen'>       
           <Header />
-          <div className='flex flex-row'>
-              <div className='mt-36 w-full h-auto flex flex-col justify-start items-center'>
-                <p className='text-7xl text-center'>Organize sua vida com o Planner!</p>
-                <p className='mt-6 text-4xl text-center'>Facilite seu trabalho utilizando a ferramenta para:</p>
-                <div className='flex flex-col items-start'>
-                    <div className='flex flex-row justify-center items-center'>
-                    <img src={Check} alt=''/>
-                    <p className='ml-4 text-primary'>Finanças</p>
+          <div className='flex flex-col lg:flex-row mx-8 lg:mx-28 my-2'>
+              <div className='mt-2 lg:mt-8 lg:mr-16 lg:w-2/3 h-auto flex flex-col justify-center items-center'>
+                <p className='w-full text-3xl lg:text-6xl text-center lg:text-left'>Organize sua vida com o Planner!</p>
+                <p className='w-full mt-8 lg:mt-12 text-xl text-left'>Facilite seu trabalho utilizando a ferramenta para:</p>
+                <div className='mt-2 lg:mt-6 w-full flex flex-col items-start justify-start'>
+                    <div className='py-1 flex flex-row justify-center items-center'>
+                        <img src={Check} alt='' className='h-6 lg:h-8'/>
+                        <p className='ml-2 lg:ml-4 text-primary text-lg lg:text-2xl'>Finanças</p>
                     </div>
-                    <div className='flex flex-row justify-center items-center'>
-                    <img src={Check} alt=''/>
-                    <p className='ml-4 text-primary'>Academia</p>
+                    <div className='py-0.5 lg:py-1 flex flex-row justify-center items-center'>
+                        <img src={Check} alt=''  className='h-6 lg:h-8'/>
+                        <p className='ml-2 lg:ml-4  text-primary text-lg lg:text-2xl'>Academia</p>
                     </div>
-                    <div className='flex flex-row justify-center items-center'>
-                    <img src={Check} alt=''/>
-                    <p className='ml-4 text-primary'>Dentre outros</p>
+                    <div className='py-0.5 lg:py-1 flex flex-row justify-center items-center'>
+                        <img src={Check} alt='' className='h-6 lg:h-8'/>
+                        <p className='py-0.5 lg:ml-2 lg:ml-4  text-primary text-lg lg:text-2xl'>Dentre outros</p>
                     </div>
                 </div>
               </div>
-              <div className='w-full flex flex-col justify-center items-end mr-24'>
-                <img className='h-128 w-128' src={StayHome} alt='Stay Home'/>
-                <button className='mt-8 w-80 h-14 text-2xl text-white bg-alert border border-black rounded-xl'>Experimente Já</button>
+              <div className='mt-4 lg:mt-8 lg:w-1/3 flex flex-col justify-center items-end'>
+                <img className='w-64 h-64 mx-auto hidden lg:block' src={StayHome} alt='Stay Home'/>
+                <button className='mt-2 lg:mt-7 w-full h-12 lg:h-14 text-2xl text-white bg-secondary rounded-lg shadow-lg'>Experimente Já!</button>
+                <div className='w-full py-1 flex justify-center'>
+                    <p className='text-secondary text-lg'>Ou</p>
+                </div>
+                <button className='w-full h-12 lg:h-14 text-2xl text-secondary bg-white border-2 border-secondary rounded-lg shadow-lg'>Registre-se</button>
+                <div className='w-full block lg:hidden'>
+                    <p className='mt-2 text-center text-lg'>Já possui conta?</p>
+                    <button className='mt-2 w-full h-12 lg:h-14 text-2xl text-white bg-secondary rounded-lg shadow-lg'>Login</button>
+                </div>
               </div>
           </div>
        </div>
