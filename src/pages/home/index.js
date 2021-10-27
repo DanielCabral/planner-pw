ed4952d34b61dac4d893eb6b6340e648a1ab419a
Dylan <dylan.oli@hotmail.com>
2021-10-27 20:59:15 -0300

    home

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,42 +1,39 @@
 import StayHome from '../../assets/stay-home.png'
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import Check from '../../assets/check.png'
 
 export default function Home() {
     return (
-       <div className='w-full h-screen'>       
-          <div className='flex flex-col lg:flex-row mx-8 lg:mx-28 my-2'>
-              <div className='mt-2 lg:mt-8 lg:mr-16 lg:w-2/3 h-auto flex flex-col justify-center items-center'>
-                <p className='w-full text-3xl lg:text-6xl text-center lg:text-left'>Organize sua vida com o Planner!</p>
-                <p className='w-full mt-8 lg:mt-12 text-xl text-left'>Facilite seu trabalho utilizando a ferramenta para:</p>
-                <div className='mt-2 lg:mt-6 w-full flex flex-col items-start justify-start'>
-                    <div className='py-1 flex flex-row justify-center items-center'>
-                        <img src={Check} alt='' className='h-6 lg:h-8'/>
-                        <p className='ml-2 lg:ml-4 text-primary text-lg lg:text-2xl'>Finanças</p>
-                    </div>
-                    <div className='py-0.5 lg:py-1 flex flex-row justify-center items-center'>
-                        <img src={Check} alt=''  className='h-6 lg:h-8'/>
-                        <p className='ml-2 lg:ml-4  text-primary text-lg lg:text-2xl'>Academia</p>
-                    </div>
-                    <div className='py-0.5 lg:py-1 flex flex-row justify-center items-center'>
-                        <img src={Check} alt='' className='h-6 lg:h-8'/>
-                        <p className='py-0.5 lg:ml-2 lg:ml-4  text-primary text-lg lg:text-2xl'>Dentre outros</p>
+        <div>
+            <div className='flex flex-col lg:flex-row mx-8 lg:mx-28 my-2'>
+                <div className='mt-2 lg:mt-8 lg:mr-16 lg:w-2/3 h-auto flex flex-col justify-center items-center'>
+                    <p className='w-full text-3xl lg:text-6xl text-center lg:text-left'>Organize sua vida com o Planner!</p>
+                    <p className='w-full mt-8 lg:mt-12 text-xl text-left'>Facilite seu trabalho utilizando a ferramenta para:</p>
+                    <div className='mt-2 lg:mt-6 w-full flex flex-col items-start justify-start'>
+                        <div className='py-1 flex flex-row justify-center items-center'>
+                            <img src={Check} alt='' className='h-6 lg:h-8' />
+                            <p className='ml-2 lg:ml-4 text-primary text-lg lg:text-2xl'>Finanças</p>
+                        </div>
+                        <div className='py-0.5 lg:py-1 flex flex-row justify-center items-center'>
+                            <img src={Check} alt='' className='h-6 lg:h-8' />
+                            <p className='ml-2 lg:ml-4  text-primary text-lg lg:text-2xl'>Academia</p>
+                        </div>
+                        <div className='py-0.5 lg:py-1 flex flex-row justify-center items-center'>
+                            <img src={Check} alt='' className='h-6 lg:h-8' />
+                            <p className='py-0.5 lg:ml-2 lg:ml-4  text-primary text-lg lg:text-2xl'>Dentre outros</p>
+                        </div>
                     </div>
                 </div>
-              </div>
-              <div className='mt-4 lg:mt-8 lg:w-1/3 flex flex-col justify-center items-end'>
-                <img className='w-64 h-64 mx-auto hidden lg:block' src={StayHome} alt='Stay Home'/>
-                <Link to='/pricing' className='mt-2 lg:mt-7 w-full h-12 lg:h-14 text-2xl text-white bg-secondary rounded-lg shadow-lg flex justify-center items-center'>Experimente Já!</Link>
-                <div className='w-full py-1 flex justify-center'>
-                    <p className='text-secondary text-lg'>Ou</p>
+                <div className='mt-4 lg:mt-8 lg:w-1/3 flex flex-col justify-center items-end'>
+                    <img className='w-64 h-64 mx-auto hidden lg:block' src={StayHome} alt='Stay Home' />
+                    <br />
+                    <Link to='/pricing'><div className='btn-third w-96 shadow-lg text-2xl mt-7'>Experimente Já!</div></Link>
+                    <div className='w-full py-1 flex justify-center'>
+                        <p className='text-secondary text-lg'>Ou</p>
+                    </div>
+                    <Link to='/login'><div className='btn-secondary-outlined w-96 shadow-lg text-2xl'>Registre-se</div></Link>
                 </div>
-                <Link to='/login' className='w-full h-12 lg:h-14 text-2xl text-secondary bg-white border-2 border-secondary rounded-lg shadow-lg  flex justify-center items-center'>Registre-se</Link>
-                <div className='w-full block lg:hidden'>
-                    <p className='mt-2 text-center text-lg'>Já possui conta?</p>
-                    <Link to='/login' className='mt-2 w-full h-12 lg:h-14 text-2xl text-white bg-secondary rounded-lg shadow-lg  flex justify-center items-center'>Login</Link>
-                </div>
-              </div>
-          </div>
-       </div>
+            </div>
+        </div>
     );
 }
--- src/pages/planners/index.js
+++ src/pages/planners/index.js
@@ -1,5 +1,5 @@
-import {Link} from 'react-router-dom'
-import {FiPlus, FiMoreVertical} from 'react-icons/fi'
+import { Link } from 'react-router-dom'
+import { FiPlus, FiMoreVertical } from 'react-icons/fi'
 export default function Planners() {
     const planners = [
         {
@@ -27,70 +27,34 @@ export default function Planners() {
     const colors = ['fourth', 'danger', 'third', 'link'];
 
     return (
-       <div className='w-full h-screen'>        
-          <div className='pt-2 lg:pt-20 px-2 lg:px-20'>
-              <div className='w-full flex flex-col lg:flex-row justify-between items-center'>
-                  <p className='font-bold text-2xl'>Seus Planners</p>
-                  <input className="border rounded-xl border-stroke focus:border-success px-6 py-2 outline-none focus:outline-none" placeholder="Buscar"></input>                  
-              </div>          
-              <div className="mt-8 grid grid-cols-2 lg:grid-cols-4 gap-4 lg:gap-8 place-items-center">
-              <Link to='new-planner' className='cursor-pointer'>
-                    <div className="w-28 h-32 lg:w-32 lg:h-40 bg-new-planner rounded-xl border-2 border-dashed tracking-widest border-secondary flex justify-center items-center">
-                        <FiPlus className="text-secondary font-serif font-bold text-5xl"/>                                                                        
-                    </div>
-              </Link>
-              <div className={`w-28 h-32 lg:w-32 lg:h-40 bg-fourth rounded-xl `}>
-                                <div className='h-28 lg:h-32 flex justify-center items-center'>
-                                    <p className="text-white font-serif font-bold text-4xl">Fin</p>                            
-                                </div>                                
-                                <div className="h-2 lg:h-8 mb-4 w-full flex justify-between items-center">
-                                    <p className="ml-1 text-white font-light text-xs">Titulo</p>
-                                    <a href='/planners'className='cursor-pointer'><FiMoreVertical className='text-white text-lg' /></a>
-                                </div>
-                            </div>
-                            <div className={`w-28 h-32 lg:w-32 lg:h-40 bg-danger rounded-xl `}>
-                                <div className='h-28 lg:h-32 flex justify-center items-center'>
-                                    <p className="text-white font-serif font-bold text-4xl">Aca</p>                            
-                                </div>                                
-                                <div className="h-2 lg:h-8 mb-4 w-full flex justify-between items-center">
-                                    <p className="ml-1 text-white font-light text-xs">Titulo</p>
-                                    <a href='/planners'className='cursor-pointer'><FiMoreVertical className='text-white text-lg' /></a>
-                                </div>
-                            </div>                          
-                            <div className={`w-28 h-32 lg:w-32 lg:h-40 bg-alert rounded-xl `}>
-                                <div className='h-28 lg:h-32 flex justify-center items-center'>
-                                    <p className="text-white font-serif font-bold text-4xl">Pro</p>                            
-                                </div>                                
-                                <div className="h-2 lg:h-8 mb-4 w-full flex justify-between items-center">
-                                    <p className="ml-1 text-white font-light text-xs">Titulo</p>
-                                    <a href='/planners'className='cursor-pointer'><FiMoreVertical className='text-white text-lg' /></a>
-                                </div>
-                            </div>
-                            <div className={`w-28 h-32 lg:w-32 lg:h-40 bg-link rounded-xl `}>
-                                <div className='h-28 lg:h-32 flex justify-center items-center'>
-                                    <p className="text-white font-serif font-bold text-4xl">Est</p>                            
-                                </div>                                
-                                <div className="h-2 lg:h-8 mb-4 w-full flex justify-between items-center">
-                                    <p className="ml-1 text-white font-light text-xs">Titulo</p>
-                                    <a href='/planners'className='cursor-pointer'><FiMoreVertical className='text-white text-lg' /></a>
-                                </div>
-                            </div>                           
+        <div className='w-full h-screen'>
+            <div className='pt-2 lg:pt-20 px-2 lg:px-20'>
+                <div className='w-full flex flex-col lg:flex-row justify-between items-center'>
+                    <p className='font-bold text-2xl'>Seus Planners</p>
+                    <input className="border rounded-xl border-stroke focus:border-success px-6 py-2 outline-none focus:outline-none" placeholder="Buscar"></input>
+                </div>
+                <div className="mt-8 grid grid-cols-2 lg:grid-cols-4 gap-4 lg:gap-8 place-items-center">
+                    <Link to='new-planner' className='cursor-pointer'>
+                        <div className="w-28 h-32 lg:w-32 lg:h-40 bg-new-planner rounded-xl border-2 border-dashed tracking-widest border-secondary flex justify-center items-center">
+                            <FiPlus className="text-secondary font-serif font-bold text-5xl" />
+                        </div>
+                    </Link>
                     {
                         planners.map((planner, i) => (
-                            <div key={planner.id} className={`w-28 h-32 lg:w-32 lg:h-40 bg-${colors[i%colors.length]} rounded-xl `}>
+                            <div key={planner.id} className={`w-28 h-32 lg:w-32 lg:h-40 bg-${colors[i % colors.length]} rounded-xl `}>
                                 <div className='h-28 lg:h-32 flex justify-center items-center'>
-                                    <p className="text-white font-serif font-bold text-4xl">{planner.type}</p>                            
-                                </div>                                
+                                    <p className="text-white font-serif font-bold text-4xl">{planner.type}</p>
+                                </div>
                                 <div className="h-2 lg:h-8 mb-4 w-full flex justify-between items-center">
                                     <p className="ml-1 text-white font-light text-xs">{planner.title}</p>
-                                    <a href='/planners'className='cursor-pointer'><FiMoreVertical className='text-white text-lg' /></a>
+                                    <a href='/planners' className='cursor-pointer'><FiMoreVertical className='text-white text-lg' /></a>
                                 </div>
                             </div>
                         ))
-                    }                                        
-              </div>    
-          </div>
+                    }
+                </div>
+            </div>
 
-       </div>
+        </div>
     );
 }
